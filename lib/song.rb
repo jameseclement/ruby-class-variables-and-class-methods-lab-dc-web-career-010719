ae801315801a175186c35586d957a4752fa01048
James Clement <jameseclement@gmail.com>
2019-01-10 03:30:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,19 @@ class Song
  @@count = 0
  @@artists = []
  @@genres = []
+ @@genre_count = {}
+ @@artist_count = {}
+ 
  attr_accessor :name, :artist, :genre
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @@artists << artist
+    @@artist_count[artist] += 1
     @genre = genre
     @@genres << genre
+    @@genre_count[genre] += 1 
     @@count += 1
   end 
 
@@ -25,5 +30,14 @@ end
 def self.genres
   @@genres.uniq
 end 
+
+def self.genre_count
+@@genre_count
+end
+
+
+def self.artist_count
+@@artist_count
+end
 
 end 
